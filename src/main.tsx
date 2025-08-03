8940db379b0d05df91b2fd802e5c3dd1598f9cf3
arpit15006 <arpit6814@gmail.com>
2025-08-03 15:37:20 +0530

    Use working development Clerk key - custom domain not resolving

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,8 @@ import "./index.css";
 import App from "./App.tsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 
-const PUBLISHABLE_KEY = "pk_live_Y2xlcmsuYXJwaXRzb2Z0d2FyZS5lbmdpbmVlciQ";
+// Using development key for now since custom domain isn't working
+const PUBLISHABLE_KEY = "pk_test_bGFyZ2Utc2t1bmstMTUuY2xlcmsuYWNjb3VudHMuZGV2JA";
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Clerk Publishable Key");
